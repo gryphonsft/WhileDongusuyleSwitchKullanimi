81dbf3f0363543d5fb0d7ea1f1354636a3796959
Abdullah BOZDAG <abdbzd01@gmail.com>
2024-09-08 23:22:48 +0300

    hi

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String isim,hayvan,kedi;
+            Console.Write("Lütfen isminizi giriniz : ");
+            isim = Console.ReadLine();
+            Console.WriteLine("Lütfen sevdiğiniz hayvanı yazınız : (Ben kedileri sevdiğim için şuan sadece kedilerle ilgili kod yazdım :) )");
+            hayvan = Console.ReadLine().ToLower();
+
+           
+
+            Console.WriteLine(isim+","+" "+hayvan+"leri"+" sever.");
+            if (hayvan == "kedi")
+            {
+                Console.WriteLine("Ooo kedilerden hoşlanıyoruz demek ki... Tekir mi,Sarman mı, Kaliko mu,Smokin mi?");
+            }
+            while (true) { 
+            Console.Write("Hangisi peki: ");
+            kedi = Console.ReadLine().ToLower();
+            switch (kedi)
+            {
+                case "tekir":
+                    Console.WriteLine("Bende tekirlere bayılırım. Çok tatlılar");
+                    break;
+              
+                case "sarman":
+                    Console.WriteLine("Bende sarmanlara bayılırım. Çok tatlılar ve biraz deli");
+                    break;
+
+                case "kaliko":
+                    Console.WriteLine("Bende kalikolara bayılırım. Aşırı tatlılar...");
+                    break;
+
+                case "smokin":
+                        Console.WriteLine("Onlar harika...");
+                        break;
+
+            }
+            Console.WriteLine("Daha var mı sevdiğin kedi : (evet, hayır)");
+            string devamDurumu = Console.ReadLine().ToLower();
+            if(devamDurumu == "evet")
+            {
+                    continue;
+            }
+                else
+                {
+                    Console.WriteLine("Kedi üstadı : Teşekkürler!");
+                    break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
